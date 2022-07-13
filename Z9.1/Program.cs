49aa0ee8e5f91465f2a4791c84a31c7561f30e5f
Nikolay <cccp_ussr@bk.ru>
2022-07-13 07:40:09 +0300

    added Z9

---
+++ Z9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = new Random().Next(10,100);
+int MaxDigit (int number)
+{  
+    int first = number / 10;
+    int second = number % 10;
+    if (first == second) return 0;
+    if (first > second) return first;
+    return second;
+}
+int maxDigit = MaxDigit(num);
+if(maxDigit == 0) Console.WriteLine($"{num} -> цифры одинаковые");
+else Console.WriteLine($"{num} -> {maxDigit}");
