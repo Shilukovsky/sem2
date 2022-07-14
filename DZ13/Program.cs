ff9be90ef0dc6144a6b40b522fe61c9dc624c0df
Nikolay <cccp_ussr@bk.ru>
2022-07-14 07:37:03 +0300

    added DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = RemoveThird(num);
+if (num / 100 > 0)
+    Console.WriteLine($"{num} третья цифра с конца {res}");
+else
+    Console.WriteLine("третьей цифры нет");
+int RemoveThird(int num)
+{
+    int ost1 = num % 1000;
+    int ost2 = num % 100;
+    return ost1 / 100 - ost2 / 100;
+}
