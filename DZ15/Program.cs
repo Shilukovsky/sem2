185a17b6be253e7d376527472e5f74b4fa9cd0f6
Nikolay <cccp_ussr@bk.ru>
2022-07-14 07:51:46 +0300

    added DZ15

---
+++ DZ15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число соответствующее дню недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+    Console.WriteLine("Выходной!");
+else if (num > 0 && num < 6)
+    Console.WriteLine("не выходной");
+else
+    Console.WriteLine("нет такого дня недели");
+
