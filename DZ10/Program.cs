d2ebbc28e7b9e2f4093209afbf854f669c715326
Nikolay <cccp_ussr@bk.ru>
2022-07-14 07:21:51 +0300

    added many tasks

---
+++ DZ10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = RemoveSecond(num);
+if (num/100 >1 && num/100 <10)
+  Console.WriteLine($"{num} второе число {res}");
+else
+  Console.WriteLine("это не трехзначное число");
+int RemoveSecond(int num)
+{
+    int ost1 = num % 100;
+    int ost2 = num % 10;   
+    return ost1 / 10 - ost2 / 10;
+}    
